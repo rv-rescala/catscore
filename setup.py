8e4c63676fbc18a9de8d606c211a1f32f90c8392
rv <m0029.swim@gmail.com>
2020-01-03 17:15:45 +0900

    update readme

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
+# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-
+from os import path
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
 # https://github.com/pypa/sampleproject/blob/master/setup.py
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
     name='catscore',
     version='0.1',
     description='Core library for cats project',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='rv',
     author_email='yo-maruya@rescala.jp',
     keywords='sample setuptools development',
