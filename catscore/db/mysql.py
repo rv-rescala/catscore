5fc87f64da58bcdef4132138c05abaea932a432d
rv <yo-maruya@rescala.jp>
2020-01-17 19:14:31 +0900

    tmp

--- catscore/db/mysql.py
+++ catscore/db/mysql.py
@@ -9,9 +9,11 @@ class MySQLConf:
     pwd: str
     db_name: str
 
-    @property
-    def connection_uri(self):
-        return f"jdbc:mysql://{self.host}:{self.port}/{self.db_name}?user={self.user}&password={self.pwd}"
+    def connection_uri(self, inteface:str):
+        if inteface == "jdbc":
+            return f"jdbc:mysql://{self.host}:{self.port}/{self.db_name}?user={self.user}&password={self.pwd}"
+        elif inteface == "pymysql":
+            return 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (self.user, self.pwd, self.host + ":" + self.port, self.db_name)
 
     @classmethod
     def from_json(cls, path):
