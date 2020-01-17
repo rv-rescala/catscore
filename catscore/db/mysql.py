21273d555962a466eccea4574820c019309171df
rv <m0029.swim@gmail.com>
2020-01-17 13:39:40 +0900

    update mysql class

--- catscore/db/mysql.py
+++ catscore/db/mysql.py
@@ -1,13 +1,26 @@
 from dataclasses import dataclass
+import json
 
 @dataclass(frozen=True)
 class MySQLConf:
     host: str
     port: str
-    usr: str
+    user: str
     pwd: str
     db_name: str
 
     @property
     def connection_uri(self):
-        return f"jdbc:mysql://{self.host}:{self.port}/{self.db_name}?user={self.usr}&password={self.pwd}"
+        return f"jdbc:mysql://{self.host}:{self.port}/{self.db_name}?user={self.user}&password={self.pwd}"
+
+    @classmethod
+    def from_json(cls, path):
+        with open(path, "r") as f:
+            j = json.load(f)
+            m = MySQLConf(
+                host=j["host"],
+                port=j["port"],
+                user=j["user"],
+                pwd=j["pwd"],
+                db_name=j["db_name"])
+        return m
