1e772417c857dcb2127accd210a423aef2eb0972
rv <m0029.swim@gmail.com>
2020-06-05 02:00:59 +0900

    u

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     
 setup(
     name='catscore',
-    version='0.3.5',
+    version='0.3.6',
     description='Core library for crawling and web scraping',
     long_description=long_description,
     long_description_content_type='text/markdown',
